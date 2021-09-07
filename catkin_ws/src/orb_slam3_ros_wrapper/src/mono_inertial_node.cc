c1da41032efa80df89261c7dff53829bae4eac77
Nithid Mahattanasin <nithid.ma@ku.th>
2021-09-07 13:15:38 +0700

    Add orbslam3, ros wrapper

---
+++ catkin_ws/src/orb_slam3_ros_wrapper/src/mono_inertial_node.cc
@@ -0,0 +1,191 @@
+/**
+* 
+* Adapted from ORB-SLAM3: Examples/ROS/src/ros_mono_inertial.cc
+*
+*/
+
+#include "common.h"
+
+using namespace std;
+
+class ImuGrabber
+{
+public:
+    ImuGrabber(){};
+    void GrabImu(const sensor_msgs::ImuConstPtr &imu_msg);
+
+    queue<sensor_msgs::ImuConstPtr> imuBuf;
+    std::mutex mBufMutex;
+};
+
+class ImageGrabber
+{
+public:
+    ImageGrabber(ORB_SLAM3::System* pSLAM, ImuGrabber *pImuGb, const bool bClahe): mpSLAM(pSLAM), mpImuGb(pImuGb), mbClahe(bClahe){}
+
+    void GrabImage(const sensor_msgs::ImageConstPtr& msg);
+    cv::Mat GetImage(const sensor_msgs::ImageConstPtr &img_msg);
+    void SyncWithImu();
+
+    queue<sensor_msgs::ImageConstPtr> img0Buf;
+    std::mutex mBufMutex;
+   
+    ORB_SLAM3::System* mpSLAM;
+    ImuGrabber *mpImuGb;
+
+    const bool mbClahe;
+    cv::Ptr<cv::CLAHE> mClahe = cv::createCLAHE(3.0, cv::Size(8, 8));
+};
+
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "Mono_Inertial");
+    ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Info);
+    if (argc > 1)
+    {
+        ROS_WARN ("Arguments supplied via command line are neglected.");
+    }
+
+    ros::NodeHandle node_handler;
+    std::string node_name = ros::this_node::getName();
+    image_transport::ImageTransport image_transport(node_handler);
+
+    std::string voc_file, settings_file;
+    node_handler.param<std::string>(node_name + "/voc_file", voc_file, "file_not_set");
+    node_handler.param<std::string>(node_name + "/settings_file", settings_file, "file_not_set");
+
+    if (voc_file == "file_not_set" || settings_file == "file_not_set")
+    {
+        ROS_ERROR("Please provide voc_file and settings_file in the launch file");       
+        ros::shutdown();
+        return 1;
+    }
+
+    node_handler.param<std::string>(node_name + "/map_frame_id", map_frame_id, "map");
+    node_handler.param<std::string>(node_name + "/pose_frame_id", pose_frame_id, "pose");
+
+    bool bEqual = false;
+    node_handler.param<bool>(node_name + "/do_equalize", bEqual, false);
+    
+    // Create SLAM system. It initializes all system threads and gets ready to process frames.
+    ORB_SLAM3::System SLAM(voc_file, settings_file, ORB_SLAM3::System::IMU_MONOCULAR, true);
+
+    ImuGrabber imugb;
+    ImageGrabber igb(&SLAM, &imugb, bEqual);
+
+    ros::Subscriber sub_imu = node_handler.subscribe("/imu", 1000, &ImuGrabber::GrabImu, &imugb); 
+    ros::Subscriber sub_img0 = node_handler.subscribe("/camera/image_raw", 100, &ImageGrabber::GrabImage, &igb);
+
+    setup_ros_publishers(node_handler, image_transport);
+
+    std::thread sync_thread(&ImageGrabber::SyncWithImu, &igb);
+
+    setup_tf_orb_to_ros(ORB_SLAM3::System::IMU_MONOCULAR);
+
+    ros::spin();
+
+    // Stop all threads
+    SLAM.Shutdown();
+
+    ros::shutdown();
+
+    return 0;
+}
+
+void ImageGrabber::GrabImage(const sensor_msgs::ImageConstPtr &img_msg)
+{
+    mBufMutex.lock();
+    if (!img0Buf.empty())
+        img0Buf.pop();
+    img0Buf.push(img_msg);
+    mBufMutex.unlock();
+}
+
+cv::Mat ImageGrabber::GetImage(const sensor_msgs::ImageConstPtr &img_msg)
+{
+    // Copy the ros image message to cv::Mat.
+    cv_bridge::CvImageConstPtr cv_ptr;
+    try
+    {
+        cv_ptr = cv_bridge::toCvShare(img_msg, sensor_msgs::image_encodings::MONO8);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+    }
+    
+    if(cv_ptr->image.type()==0)
+    {
+        return cv_ptr->image.clone();
+    }
+    else
+    {
+        std::cout << "Error type" << std::endl;
+        return cv_ptr->image.clone();
+    }
+}
+
+void ImageGrabber::SyncWithImu()
+{
+    while(1)
+    {
+        if (!img0Buf.empty()&&!mpImuGb->imuBuf.empty())
+        {
+            cv::Mat im;
+            double tIm = 0;
+            ros::Time current_frame_time;
+
+            tIm = img0Buf.front()->header.stamp.toSec();
+            if(tIm>mpImuGb->imuBuf.back()->header.stamp.toSec())
+                continue;
+            
+            {
+            this->mBufMutex.lock();
+            im = GetImage(img0Buf.front());
+            current_frame_time = img0Buf.front()->header.stamp;
+            img0Buf.pop();
+            this->mBufMutex.unlock();
+            }
+
+            vector<ORB_SLAM3::IMU::Point> vImuMeas;
+            mpImuGb->mBufMutex.lock();
+            if (!mpImuGb->imuBuf.empty())
+            {
+                // Load imu measurements from buffer
+                vImuMeas.clear();
+                while(!mpImuGb->imuBuf.empty() && mpImuGb->imuBuf.front()->header.stamp.toSec() <= tIm)
+                {
+                    double t = mpImuGb->imuBuf.front()->header.stamp.toSec();
+                    cv::Point3f acc(mpImuGb->imuBuf.front()->linear_acceleration.x, mpImuGb->imuBuf.front()->linear_acceleration.y, mpImuGb->imuBuf.front()->linear_acceleration.z);
+                    cv::Point3f gyr(mpImuGb->imuBuf.front()->angular_velocity.x, mpImuGb->imuBuf.front()->angular_velocity.y, mpImuGb->imuBuf.front()->angular_velocity.z);
+                    vImuMeas.push_back(ORB_SLAM3::IMU::Point(acc, gyr, t));
+                    mpImuGb->imuBuf.pop();
+                }
+            }
+            mpImuGb->mBufMutex.unlock();
+            if(mbClahe)
+                mClahe->apply(im,im);
+
+            // Main algorithm runs here
+            cv::Mat Tcw = mpSLAM->TrackMonocular(im, tIm, vImuMeas);
+
+            publish_ros_pose_tf(Tcw, current_frame_time, ORB_SLAM3::System::IMU_MONOCULAR);
+
+            publish_ros_tracking_mappoints(mpSLAM->GetTrackedMapPoints(), current_frame_time);
+
+            // publish_ros_tracking_img(mpSLAM->GetCurrentFrame(), current_frame_time);
+        }
+
+        std::chrono::milliseconds tSleep(1);
+        std::this_thread::sleep_for(tSleep);
+    }
+}
+
+void ImuGrabber::GrabImu(const sensor_msgs::ImuConstPtr &imu_msg)
+{
+    mBufMutex.lock();
+    imuBuf.push(imu_msg);
+    mBufMutex.unlock();
+    return;
+}
---
+++ catkin_ws/src/orb_slam3_ros_wrapper/src/mono_node.cc
@@ -0,0 +1,95 @@
+/**
+* 
+* Adapted from ORB-SLAM3: Examples/ROS/src/ros_mono.cc
+*
+*/
+
+#include "common.h"
+
+using namespace std;
+
+class ImageGrabber
+{
+public:
+    ImageGrabber(ORB_SLAM3::System* pSLAM):mpSLAM(pSLAM){}
+
+    void GrabImage(const sensor_msgs::ImageConstPtr& msg);
+
+    ORB_SLAM3::System* mpSLAM;
+};
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "Mono");
+    ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Info);
+    if (argc > 1)
+    {
+        ROS_WARN ("Arguments supplied via command line are neglected.");
+    }
+
+    ros::NodeHandle node_handler;
+    std::string node_name = ros::this_node::getName();
+    image_transport::ImageTransport image_transport(node_handler);
+
+    std::string voc_file, settings_file;
+    node_handler.param<std::string>(node_name + "/voc_file", voc_file, "file_not_set");
+    node_handler.param<std::string>(node_name + "/settings_file", settings_file, "file_not_set");
+
+    if (voc_file == "file_not_set" || settings_file == "file_not_set")
+    {
+        ROS_ERROR("Please provide voc_file and settings_file in the launch file");       
+        ros::shutdown();
+        return 1;
+    }
+
+    node_handler.param<std::string>(node_name + "/map_frame_id", map_frame_id, "map");
+    node_handler.param<std::string>(node_name + "/pose_frame_id", pose_frame_id, "pose");
+
+    bool enable_pangolin;
+    node_handler.param<bool>(node_name + "/enable_pangolin", enable_pangolin, true);
+
+    // Create SLAM system. It initializes all system threads and gets ready to process frames.
+    ORB_SLAM3::System SLAM(voc_file, settings_file, ORB_SLAM3::System::MONOCULAR, enable_pangolin);
+    ImageGrabber igb(&SLAM);
+
+    ros::Subscriber sub_img0 = node_handler.subscribe("/camera/image_raw", 1, &ImageGrabber::GrabImage, &igb);
+
+    setup_ros_publishers(node_handler, image_transport);
+
+    setup_tf_orb_to_ros(ORB_SLAM3::System::MONOCULAR);
+
+    ros::spin();
+
+    // Stop all threads
+    SLAM.Shutdown();
+
+    ros::shutdown();
+
+    return 0;
+}
+
+void ImageGrabber::GrabImage(const sensor_msgs::ImageConstPtr& msg)
+{
+    // Copy the ros image message to cv::Mat.
+    cv_bridge::CvImageConstPtr cv_ptr;
+    try
+    {
+        cv_ptr = cv_bridge::toCvShare(msg);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+        return;
+    }
+
+    // Main algorithm runs here
+    cv::Mat Tcw = mpSLAM->TrackMonocular(cv_ptr->image, cv_ptr->header.stamp.toSec());
+
+    ros::Time current_frame_time = msg->header.stamp;
+
+    publish_ros_pose_tf(Tcw, current_frame_time, ORB_SLAM3::System::MONOCULAR);
+
+    publish_ros_tracking_mappoints(mpSLAM->GetTrackedMapPoints(), current_frame_time);
+
+    // publish_ros_tracking_img(mpSLAM->GetCurrentFrame(), current_frame_time);
+}
